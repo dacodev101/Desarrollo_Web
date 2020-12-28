a87e5cd18fbf17656a10f99f0348f8861c45ba83
Daniel <daniel199728@hotmail.com>
2020-12-28 16:35:28 -0500

    Agregando al JS funciones de iluminacion de id del html y css

--- simon_dice.js
+++ simon_dice.js
@@ -8,11 +8,12 @@ class Juego {
   constructor() {
     this.inicializar()
     this.generarSecuencia()
+    this.siguienteNivel()
   }
 
   inicializar() {
     btnEmpezar.classList.add('hide')
-    this.nivel = 1
+    this.nivel = 7
     this.colores = {
       celeste,
       violeta,
@@ -20,12 +21,45 @@ class Juego {
       verde
     }
   }
-  
-  generarSecuencia(){
+
+  generarSecuencia() {
     this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+  }
+
+  siguienteNivel() {
+    this.iluminarSecuencia()
+  }
+
+  transformarNumeroAColor(numero) {
+    switch (numero) {
+      case 0:
+        return 'celeste'
+      case 1:
+        return 'violeta'
+      case 2:
+        return 'naranja'
+      case 3:
+        return 'verde'
+    }
+  }
+
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      const color = this.transformarNumeroAColor(this.secuencia[i])
+      setTimeout(() => this.iluminarColor(color), 1000 * i)
+    }
+  }
+
+  iluminarColor(color) {
+    this.colores[color].classList.add('light')
+    setTimeout(() => this.apagarColor(color), 350)
+  }
+
+  apagarColor(color) {
+    this.colores[color].classList.remove('light')
   }
 }
 
 function empezarJuego() {
-  window.juego = new Juego()
+  var juego = new Juego()
 }
